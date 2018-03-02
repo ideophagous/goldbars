364dbd9a3916935928a12ffc18336431713ec37a
Ideophagous <antontheog@gmail.com>
2018-03-02 10:43:43 +0000

    adding a docstring to the test class, adding a comment in patgbuilder function, adding test_pathbuilder function

--- sheffield_test.py
+++ sheffield_test.py
@@ -3,12 +3,20 @@ import unittest
 import random
 
 class TestSheffield(unittest.TestCase):
+    """
+    an extension class for testing sheffield.py program
+
+    functions:
+
+    pathbuilder: returns a path containing m
+    villages and n towns in random order.
+    """
     def pathbuilder(self,m,n):
         v = 0
         t = 0
         path = []
         for i in range(m+n):
-            r = randint(0,1)
+            r = randint(0,1) #generating 0 or 1 randomly
             if(r==0):
                 path.append(chr(v+97))
                 v+=1
@@ -26,6 +34,9 @@ class TestSheffield(unittest.TestCase):
                     v+=1
                 break
         return path
+
+    def test_pathbuilder(self,m,n):
+        pass
 
     def test_compare(self,p1,p2):
         pass
